6647450ea98421633421e4208268485a5042907c
bunhojun <ponnponn.p@gmail.com>
2023-05-07 17:45:57 +0900

    implement in main

--- my_assistant/main.py
+++ my_assistant/main.py
@@ -1,65 +1,46 @@
-import sounddevice as sd
-import speech_recognition as sr
-import numpy as np
 from chat_assistant import ChatAssistant
 from text_to_speech_player import TextToSpeechPlayer
+from speech_listener import SpeechListener
 
 
 def is_keyword(text: str):
     return 'OK' in text and ('ブンブン' in text or 'ぶんぶん' in text)
 
 
-initial_duration = 8
+def trim_keyword(text: str):
+    return text.replace('OK', '').replace('ブンブン', '').replace('ぶんぶん', '')
+
+
+initial_duration = 4
 
 
 def main():
-    sample_rate = 16000  # 16 kHz
     chunk_duration = initial_duration
-    chunk_samples = chunk_duration * sample_rate
     is_active = False
-    is_responding = False
 
-    r = sr.Recognizer()
+    sl = SpeechListener(chunk_duration=chunk_duration)
     assistant = ChatAssistant()
     ttsp = TextToSpeechPlayer('ja')
 
-    def callback(in_data, _a, _b, _c):
-        nonlocal is_active, chunk_duration, is_responding
-        audio_data = np.frombuffer(in_data, dtype=np.int16)
-        source = sr.AudioData(audio_data.tobytes(), sample_rate, 2)
-        try:
-            text: str = r.recognize_google(source, language='ja-JP')
-            if not is_active:
-                print('not active: ', text)
-            if 'ありがとう' in text:
-                is_active = False
-                is_responding = True
-                result = assistant.chat(text)
-                ttsp.play_text(result)
-                is_responding = False
-                chunk_duration = initial_duration
-                return
-            if not is_responding and (is_active or is_keyword(text)):
-                is_active = True
-                chunk_duration = 18
-                is_responding = True
-                result = assistant.chat(text)
-                ttsp.play_text(result)
-                is_responding = False
+    def callback(text: str):
+        nonlocal is_active, chunk_duration
+        if not is_active:
+            print('not active: ', text)
+        if is_active:
+            is_active = False
+            result = assistant.chat(text)
+            ttsp.play_text(result)
+            chunk_duration = initial_duration
+            return
+        if is_keyword(text):
+            ttsp.play_text('はい')
+            chunk_duration = 10
+            is_active = True
 
-        except sr.UnknownValueError:
-            pass
-
-    with sd.InputStream(
-        samplerate=sample_rate,
-        channels=1,
-        dtype=np.int16,
-        blocksize=chunk_samples,
-        callback=callback,
-    ):
+    with sl.use_stream(callback):
         print("Listening...")
         while True:
-            sd.sleep(chunk_duration * 1000)
+            sl.record(chunk_duration)
 
 
 if __name__ == "__main__":
